9f76768b33a82d77844dcf616ba3dab100289eb6
Stock05 <qace756@gmail.com>
2024-04-05 17:55:33 +0900

    메뉴 및 수량 추가

--- kiosk/admin.py
+++ kiosk/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from kiosk.models import Question,Choice
 # Register your models here.
+
+
+@admin.register(Question)
+class KioskAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Choice)
+class KioskAdmin(admin.ModelAdmin):
+    list_display=('question','drink_name','price')
+
--- kiosk/models.py
+++ kiosk/models.py
@@ -9,13 +9,14 @@ from django.contrib import admin
 class Question(models.Model):
     drinks_type = models.CharField(max_length=200)    
     def __str__(self):
-        text = self.question_text 
+        text = self.drinks_type 
         return text    
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     drink_name = models.CharField(max_length=200)
-    price = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)    
+
     def __str__(self):        
-        return self.choice_text
+        return self.drink_name
